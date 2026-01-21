254ae52e3c2ddc2254e5f8eacf98c7b86036ce79
Gabriel Gavrilov <gavrilog@fcc.ca>
2026-01-21 16:01:25 -0600

    update

--- src/main/java/com/gabrielgavrilov/mocha/MochaReflectionTools.java
+++ src/main/java/com/gabrielgavrilov/mocha/MochaReflectionTools.java
@@ -1,4 +1,22 @@
 package com.gabrielgavrilov.mocha;
 
+import java.lang.reflect.Field;
+
 public class MochaReflectionTools {
+
+    public static Object hydrateClass(Class<?> clazz, Object... data) {
+        try {
+            Object classInstance = clazz.getDeclaredConstructor().newInstance();
+            Field[] fields = clazz.getDeclaredFields();
+            for (Field field : fields) {
+                field.setAccessible(true);
+                field.set(classInstance, data[0]);
+            }
+
+            return classInstance;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
